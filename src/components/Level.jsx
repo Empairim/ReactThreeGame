25c01e6a7ea2bcc8ad165059947b7884378bee79
Empairim <titusfaulknertull@yahoo.com>
2024-05-08 14:00:45 -0400

    added radomized colors to obstacles and tile maps

--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -1,18 +1,57 @@
 import { RigidBody } from "@react-three/rapier";
 import * as THREE from "three";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useFrame } from "@react-three/fiber";
 
-// Materials and Geometries
-// 1. Floor
-// 2. Obstacle
-// 3. Wall
-// Creating one BoxGeometry and reusing it for all the meshes in the scene is a good practice. This way, we can save memory and improve performance.
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1); //
-const floor1Material = new THREE.MeshStandardMaterial({ color: "green" });
-const floor2Material = new THREE.MeshStandardMaterial({ color: "blue" });
-const obstacleMaterial = new THREE.MeshStandardMaterial({ color: "red" });
-const wallMaterial = new THREE.MeshStandardMaterial({ color: "slategrey" });
+const floor1Material = new THREE.MeshStandardMaterial({
+  color: getRandomColor(),
+});
+const floor2Material = [
+  new THREE.MeshStandardMaterial({
+    color: getRandomColor(),
+  }),
+  new THREE.MeshStandardMaterial({
+    color: getRandomColor(),
+  }),
+  new THREE.MeshStandardMaterial({
+    color: getRandomColor(),
+  }),
+  new THREE.MeshStandardMaterial({
+    color: getRandomColor(),
+  }),
+  new THREE.MeshStandardMaterial({
+    color: getRandomColor(),
+  }),
+];
+const obstacleMaterials = [
+  new THREE.MeshStandardMaterial({ color: getRandomColor() }),
+  new THREE.MeshStandardMaterial({ color: getRandomColor() }),
+  new THREE.MeshStandardMaterial({ color: getRandomColor() }),
+  new THREE.MeshStandardMaterial({ color: getRandomColor() }),
+  new THREE.MeshStandardMaterial({ color: getRandomColor() }),
+];
+const wallMaterial = new THREE.MeshStandardMaterial({
+  color: getRandomColor(),
+});
+
+function getRandomMaterial() {
+  return obstacleMaterials[
+    Math.floor(Math.random() * obstacleMaterials.length)
+  ];
+}
+function getRandomFloorMaterial() {
+  return floor2Material[Math.floor(Math.random() * floor2Material.length)];
+}
 
 function BlockStart({ position = [0, 0, 0] }) {
   return (
@@ -28,36 +67,84 @@ function BlockStart({ position = [0, 0, 0] }) {
     </group>
   );
 }
-
-function BlockSpinner({ position = [0, 0, 0] }) {
+export function BlockSpinner({ position = [0, 0, 0] }) {
   const obstacleRef = useRef();
-  // Rotating the obstacle using useFrame This hook is used to rotate the obstacle around the Y-axis. The rotation is calculated using the clock from the useFrame hook. The obstacle is rotated by setting the quaternion of the obstacleRef. The rotation is calculated using the setFromEuler method of the Quaternion class. The rotation is applied to the Y-axis using the Euler class. The rotation is applied to the obstacle using the copy method.
+  const [speed] = useState(
+    () => (Math.random() + 0.2) * (Math.random() < 0.5 ? -1 : 1)
+  );
+
   useFrame((state) => {
     const time = state.clock.getElapsedTime();
+
     const rotation = new THREE.Quaternion();
-    rotation.setFromEuler(new THREE.Euler(0, time, 0));
-    obstacleRef.current.quaternion.copy(rotation);
+    rotation.setFromEuler(new THREE.Euler(0, time * speed, 0));
+    obstacleRef.current.setNextKinematicRotation(rotation);
   });
+
   return (
     <group position={position}>
-      {/* Floor */}
       <mesh
         geometry={boxGeometry}
-        material={floor2Material}
+        material={getRandomFloorMaterial()}
         position={[0, -0.1, 0]}
         scale={[4, 0.2, 4]}
         receiveShadow
       />
-      {/* Obstacle */}
-      <mesh
+      <RigidBody
         ref={obstacleRef}
+        type="kinematicPosition"
+        position={[0, 0.3, 0]}
+        restitution={0.2}
+        friction={0}
+      >
+        <mesh
+          geometry={boxGeometry}
+          material={getRandomMaterial()}
+          scale={[3.5, 0.3, 0.3]}
+          castShadow
+          receiveShadow
+        />
+      </RigidBody>
+    </group>
+  );
+}
+
+function BlockLimbo({ position = [0, 0, 0] }) {
+  const obstacleRef = useRef();
+  const [timeOffset] = useState(() => Math.random() * Math.PI * 2);
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+    const y = Math.sin(time * timeOffset) + 1.15;
+    obstacleRef.current.setNextKinematicTranslation({
+      x: 0,
+      y: y,
+      z: position[2],
+    });
+  });
+  return (
+    <group position={position}>
+      <mesh
         geometry={boxGeometry}
-        material={obstacleMaterial}
-        position={[0, 0.5, 0]}
-        scale={[3.5, 0.3, 0.3]}
-        castShadow
+        material={getRandomFloorMaterial()}
+        position={[0, -0.1, 0]}
+        scale={[4, 0.2, 4]}
         receiveShadow
       />
+      <RigidBody
+        ref={obstacleRef}
+        type="kinematicPosition"
+        position={[0, 0.3, 0]}
+        restitution={0.2}
+        friction={0}
+      >
+        <mesh
+          geometry={boxGeometry}
+          material={getRandomMaterial()}
+          scale={[3.5, 0.3, 0.3]}
+          castShadow
+          receiveShadow
+        />
+      </RigidBody>
     </group>
   );
 }
@@ -65,15 +152,9 @@ function BlockSpinner({ position = [0, 0, 0] }) {
 export default function Level() {
   return (
     <>
-      <RigidBody
-        type="kinematicPosition"
-        position={[0, 0.3, 0]}
-        restitution={0.2}
-        friction={0}
-      >
-        <BlockStart position={[0, 0, 4]} />
-        <BlockSpinner position={[0, 0, 0]} />
-      </RigidBody>
+      <BlockStart position={[0, 0, 8]} />
+      <BlockSpinner position={[0, 0, 4]} />
+      <BlockLimbo position={[0, 0, 0]} />
     </>
   );
 }
