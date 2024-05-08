ccb790e324823aa141bb8a998cfb868a67c09828
Empairim <titusfaulknertull@yahoo.com>
2024-05-08 17:11:47 -0400

    added end block goal symbol and the ability for it to bob up and down

--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -1,17 +1,10 @@
 import { RigidBody } from "@react-three/rapier";
 import * as THREE from "three";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { useFrame } from "@react-three/fiber";
-
-function getRandomColor() {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
+import { useGLTF } from "@react-three/drei";
+////////
+//Geometry and Materials
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1); //
 const floor1Material = new THREE.MeshStandardMaterial({
   color: getRandomColor(),
@@ -43,6 +36,16 @@ const obstacleMaterials = [
 const wallMaterial = new THREE.MeshStandardMaterial({
   color: getRandomColor(),
 });
+////////
+// functions to randomize colors
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 function getRandomMaterial() {
   return obstacleMaterials[
@@ -52,7 +55,8 @@ function getRandomMaterial() {
 function getRandomFloorMaterial() {
   return floor2Material[Math.floor(Math.random() * floor2Material.length)];
 }
-
+///////////
+//Game blocks
 function BlockStart({ position = [0, 0, 0] }) {
   return (
     <group position={position}>
@@ -78,11 +82,12 @@ export function BlockSpinner({ position = [0, 0, 0] }) {
 
     const rotation = new THREE.Quaternion();
     rotation.setFromEuler(new THREE.Euler(0, time * speed, 0));
-    obstacleRef.current.setNextKinematicRotation(rotation);
+    obstacleRef.current?.setNextKinematicRotation(rotation);
   });
 
   return (
     <group position={position}>
+      {/* Floor */}
       <mesh
         geometry={boxGeometry}
         material={getRandomFloorMaterial()}
@@ -115,14 +120,15 @@ function BlockLimbo({ position = [0, 0, 0] }) {
   useFrame((state) => {
     const time = state.clock.getElapsedTime();
     const y = Math.sin(time * timeOffset) + 1.15;
-    obstacleRef.current.setNextKinematicTranslation({
-      x: 0,
-      y: y,
+    obstacleRef.current?.setNextKinematicTranslation({
+      x: position[0],
+      y: position[1] + y,
       z: position[2],
     });
   });
   return (
     <group position={position}>
+      {/* Floor */}
       <mesh
         geometry={boxGeometry}
         material={getRandomFloorMaterial()}
@@ -149,12 +155,95 @@ function BlockLimbo({ position = [0, 0, 0] }) {
   );
 }
 
+function BlockAxe({ position = [0, 0, 0] }) {
+  const obstacleRef = useRef();
+  const [timeOffset] = useState(() => Math.random() * Math.PI * 2);
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+    const x = Math.sin(time * timeOffset) * 1.25;
+    obstacleRef.current?.setNextKinematicTranslation({
+      x: position[0] + x,
+      y: position[1] + 0.75,
+      z: position[2],
+    });
+  });
+  return (
+    <group position={position}>
+      {/* Floor */}
+      <mesh
+        geometry={boxGeometry}
+        material={getRandomFloorMaterial()}
+        position={[0, -0.1, 0]}
+        scale={[4, 0.2, 4]}
+        receiveShadow
+      />
+      <RigidBody
+        ref={obstacleRef}
+        type="kinematicPosition"
+        position={[0, 0.3, 0]}
+        restitution={0.2}
+        friction={0}
+      >
+        <mesh
+          geometry={boxGeometry}
+          material={getRandomMaterial()}
+          scale={[1.5, 1.5, 0.3]}
+          castShadow
+          receiveShadow
+        />
+      </RigidBody>
+    </group>
+  );
+}
+
+function BlockEnd({ position = [0, 0, 0] }) {
+  const goal = useGLTF("./goalGold.glb");
+  console.log(goal);
+  useEffect(() => {
+    goal.scene.traverse((node) => {
+      if (node.isMesh) {
+        node.castShadow = true;
+        node.receiveShadow = true;
+      }
+    });
+  }, [goal.scene]);
+
+  const goalRef = useRef();
+  useFrame(({ clock }) => {
+    goalRef.current?.setNextKinematicTranslation({
+      x: 0,
+      y: 0.5 + Math.sin(clock.getElapsedTime()) * 0.1,
+      z: 0,
+    });
+  });
+
+  return (
+    <group position={position}>
+      {/* Floor */}
+      <mesh
+        geometry={boxGeometry}
+        material={floor1Material}
+        position={[0, 0, 0]}
+        scale={[4, 0.2, 4]}
+        receiveShadow
+      />
+      {/* Goal */}
+      <RigidBody ref={goalRef} type="kinematicPosition" position={[0, 0.5, 0]}>
+        <primitive object={goal.scene} scale={[0.5, 0.5, 0.5]} />
+      </RigidBody>
+    </group>
+  );
+}
+////////
+// Level component
 export default function Level() {
   return (
     <>
-      <BlockStart position={[0, 0, 8]} />
-      <BlockSpinner position={[0, 0, 4]} />
-      <BlockLimbo position={[0, 0, 0]} />
+      <BlockStart position={[0, 0, 16]} />
+      <BlockSpinner position={[0, 0, 12]} />
+      <BlockLimbo position={[0, 0, 8]} />
+      <BlockAxe position={[0, 0, 4]} />
+      <BlockEnd position={[0, 0, 0]} />
     </>
   );
 }
